4b9218233454fa8fa8ab31a015b3c842753fc69f
aka-Ron <142239163+aka-Ron@users.noreply.github.com>
2023-08-26 19:47:37 +0300

    Done!

---
+++ MT.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+    strings: ['The CEO got Bored.', 'Maybe You!', 'I need someone to call me!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
